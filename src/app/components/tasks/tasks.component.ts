00d74eb2f494d4fc8064c2e0d898e8c79e7e2feb
fennix530 <fennix530@gmai.com>
2022-12-17 16:17:48 -0300

    Server tasks

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from '../../service/task.service';
+import { Task } from '../../Task'
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(
+    private taskService:TaskService
+  ) { }
+
+  ngOnInit(): void {
+    // Like promise
+   this.taskService.getTasks().subscribe((tasks)=>(
+   this.tasks = tasks
+   ));
+  }
+
+}
---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,41 @@
+import {Task} from"./Task"
+
+export const TASKS: Task[] =[
+{
+    id: 0,
+    text: "Mi primer modulo de angular",
+    day: "Diciembre 5 a la 17:36",
+    reminder: true,
+
+},
+
+{
+    id: 1,
+    text: "Hacer las tarea",
+    day: "Diciembre 7 a la 17:36",
+    reminder: true
+
+},
+
+{
+    id: 2,
+    text: "Ir a la playa el fin ",
+    day: "Diciembre 9 a la 10:00",
+    reminder: true
+},
+
+{
+    id: 3,
+    text: "Hacer ejercicio todo los dias",
+    day: "Diciembre 5 a 30, a la 17:36",
+    reminder: true
+},
+
+{
+    id: 4,
+    text: "Sacar a Rocky",
+    day: "Todo los dias a la 19:30",
+    reminder: true
+}
+
+]
